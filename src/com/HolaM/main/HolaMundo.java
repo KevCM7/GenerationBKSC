c54753278dc28bcf56d495868ccb0e53f8641053
KevCM7 <kevcm1010@gmail.com>
2018-06-12 12:11:02 -0500

    Segunda version

--- src/com/HolaM/main/HolaMundo.java
+++ src/com/HolaM/main/HolaMundo.java
@@ -3,7 +3,8 @@ package com.HolaM.main;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.print("Hola Mundo");
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo1111");
 		// TODO Auto-generated method stub
 
 	}
